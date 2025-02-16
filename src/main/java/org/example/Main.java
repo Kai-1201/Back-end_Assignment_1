eab842b18349d7337fce46d521c827f20b34243d
Karim <karim.assimov@narxoz.kz>
2025-02-16 23:26:21 +0500

    Set up project structure

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Laptop laptop = new Laptop();
+        laptop.setName ("Aser");
+        System.out.println(laptop.getName());
+    }
+}
