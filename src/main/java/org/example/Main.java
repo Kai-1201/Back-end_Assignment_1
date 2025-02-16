1cc8cdaaf6edb3dad0a8c7d4859cd0fdf65bdf2e
Karim <karim.assimov@narxoz.kz>
2025-02-16 23:35:28 +0500

    Add second bean implementation

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,9 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Main {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
-        Laptop laptop = context.getBean(Laptop.class);
-        System.out.println(laptop.getName());
+        Laptop laptop_1 = context.getBean("laptop_1", Laptop.class);
+        Laptop laptop_2 = context.getBean("laptop_2", Laptop.class);
+        System.out.println(laptop_1.getName());
+        System.out.println(laptop_2.getName());
     }
 }
--- src/main/java/org/example/ProjectConfig.java
+++ src/main/java/org/example/ProjectConfig.java
@@ -3,15 +3,23 @@ package org.example;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class ProjectConfig {
 
     @Bean
-    public Laptop laptop() {
-        Laptop laptop = new Laptop();
-        laptop.setName("Acer");
-        return laptop;
+    @Primary
+    public Laptop laptop_1() {
+        Laptop laptop_1 = new Laptop();
+        laptop_1.setName("Acer");
+        return laptop_1;
+    }
+    @Bean
+    public Laptop laptop_2() {
+        Laptop laptop_2 = new Laptop();
+        laptop_2.setName("Asus");
+        return laptop_2;
     }
 }
 
